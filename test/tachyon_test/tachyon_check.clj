74e8d197ffb08bc3f5a6d36c66f1e61f21bff252
David Capwell <dcapwell@gmail.com>
2014-11-29 23:15:00 -0800

    working on generative java tests

--- test/tachyon_test/tachyon_check.clj
+++ test/tachyon_test/tachyon_check.clj
@@ -3,7 +3,10 @@
            [clojure.test.check.generators :as gen]
            [clojure.test.check.properties :as prop]
            [clojure.test.check.clojure-test :refer (defspec)]
-           [clojure.string :as string]))
+           [clojure.string :as string]
+           [tachyon-test.config :refer [config]]
+           [tachyon-test.core :as core]
+           [tachyon-test.tachyon :as tachyon]))
 
 (defn prefix
   [p rest]
@@ -18,9 +21,36 @@
 (defmethod path clojure.lang.PersistentVector [parts]
   (prefix "/" (string/join "/" parts)))
 
-(defn path-gen [] (gen/vector gen/string))
+(defmulti perform-action (fn [action] [(:type action) (:via action)]))
+(defmethod perform-action [::create-file ::java] 
+  [action]
+  (with-open [fs (tachyon/create-filesystem)]
+    (try
+      (let [file (.createFile fs (:path action))]
+        ; no exception was thrown, so action was ok
+        true)
+      (catch java.io.IOException e
+        (case (class (.getCause e))
+          #=tachyon.thrift.InvalidPathException true
+          #=tachyon.thrift.FileAlreadyExistException true
+          false)))))
+
+(defn path-gen 
+  [] 
+  (->> (gen/not-empty gen/string)
+       (gen/such-that #(not (.contains % "/")))
+       (gen/vector)
+       (gen/not-empty)
+       (gen/fmap path)))
 
 (defspec path-string
-  100
+  500
   (prop/for-all [v (path-gen)]
     (= (path v) (path (path v)))))
+
+(defspec create-file
+  500
+  (prop/for-all [v (path-gen)]
+    (perform-action {:type ::create-file 
+                     :via ::java 
+                     :path v})))
